317f0a3a1b7cfc7cd851d481947dd8d985e35e88
vadim_kozelsky <102738166+ValdemarKo@users.noreply.github.com>
2022-09-06 19:23:44 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,15 @@ class User < ApplicationRecord
 
   before_validation :nickname_downcase
 
-  has_many :questions, dependent: :delete_all
-
   NICKNAME_REGEXP = /\A[a-z0-9\_]+\z/
   EMAIL_REGEXP = /\A[a-z0-9]+@[a-z0-9]+\.[a-z]+\z/
   
   validates :email, presence: true, uniqueness: true, format: { with:EMAIL_REGEXP}
   validates :nickname, presence: true, length: {maximum: 40}, 
   uniqueness: true, format: {with: NICKNAME_REGEXP} 
+  has_many :questions, dependent: :delete_all
+  
+  private
 
   def nickname_downcase 
     nickname.downcase!
