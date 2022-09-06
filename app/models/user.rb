9be05334201e622f265d3c279ba733180e87b504
vadim_kozelsky <vadim.kozelskij@mail.ru>
2022-09-06 10:27:55 +0300

    add validate model(user, question), add errors questionviews form, add index_to_users migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,13 @@ class User < ApplicationRecord
   before_save :nickname_downcase
 
   has_many :questions, dependent: :delete_all
+
+  NICKNAME_REGEXP = /\A[a-z0-9\_]+\z/
+  EMAIL_REGEXP = /\A[a-z0-9]+@[a-z0-9]+\.[a-z]+\z/
   
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEXP }
+  validates :nickname, presence: true, length: { maximum: 40 }, 
+  uniqueness: true, format: { with: NICKNAME_REGEXP } 
 
   def nickname_downcase 
     nickname.downcase!
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_153638) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_210810) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -28,7 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_153638) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
 end
