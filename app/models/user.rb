1708bd7804f0d3ffa8297c5a884dd5f98957ae7d
vadim_kozelsky <102738166+ValdemarKo@users.noreply.github.com>
2022-09-06 19:18:17 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
 
-  before_save :nickname_downcase
+  before_validation :nickname_downcase
 
   has_many :questions, dependent: :delete_all
 
   NICKNAME_REGEXP = /\A[a-z0-9\_]+\z/
   EMAIL_REGEXP = /\A[a-z0-9]+@[a-z0-9]+\.[a-z]+\z/
   
-  validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEXP }
-  validates :nickname, presence: true, length: { maximum: 40 }, 
-  uniqueness: true, format: { with: NICKNAME_REGEXP } 
+  validates :email, presence: true, uniqueness: true, format: { with:EMAIL_REGEXP}
+  validates :nickname, presence: true, length: {maximum: 40}, 
+  uniqueness: true, format: {with: NICKNAME_REGEXP} 
 
   def nickname_downcase 
     nickname.downcase!
